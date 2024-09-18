f653daf1f732f72c5ba3978b8e316791e9cac025
Georgi-S <106542204+Georgi-S@users.noreply.github.com>
2024-09-18 19:23:40 +0200

    refine design

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -6,11 +6,11 @@ export default function AdminPage() {
     <div className="flex min-h-screen flex-col">
       <main className="flex-1">
         <section className="container flex flex-col items-center justify-center gap-6 py-12 md:py-24 lg:py-32">
-          <div className="space-y-4 text-center">
+          <div className="space-y-4 typewriter drop-shadow  text-center">
             <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl">
               Admin Dashboard
             </h1>
-            <p className="max-w-[700px] text-muted-foreground md:text-xl/relaxed">
+            <p className="max-w-[700px] typewriter text-muted-foreground md:text-xl/relaxed">
               Manage your projects, users, and expenses.
             </p>
           </div>
